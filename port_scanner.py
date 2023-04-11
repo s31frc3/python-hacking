7ab65ace1fc4051099e38040b0441f0d4ceb26c5
Andrei <zenin.an@do-student.ru>
2023-04-11 19:58:43 +0300

    made progress bar and few changes

--- port_scanner.py
+++ port_scanner.py
@@ -1,17 +1,41 @@
 import socket as s
 from termcolor import colored
 from IPy import IP
+from progress.bar import IncrementalBar
 
-def scan(ip_addr, port):
+def scan(target, port):
+    converted_ip = check_ip(target)
+    print(colored(f'\n scanning target {str(target)}', 'yellow'))
+    with IncrementalBar('Scanning ports:', max=port) as bar:
+        for port in range(1, port):
+            scan_port(converted_ip, port, bar)
+
+def check_ip(ip):
+    try:
+        IP(ip)
+        return ip
+    except ValueError:
+        return s.gethostbyname(ip)
+
+def scan_port(ip_addr, port, bar):
     try:
         sock = s.socket()
         sock.settimeout(0.5)
         sock.connect((ip_addr, port))
-        print(colored(f'[+] Port {port} is open!', 'green'))
+        print(colored(f'\n[+] Port {port} is open!', 'green'))
+        sock.close()
     except:
         # print(colored(f'[-] Port {port} is closed', 'red'))
         pass
+    finally:
+        bar.next()
 
-ip_addr = input(colored('[+] Enter target to scan: ', 'blue'))
-for port in range(1,65535):
-    scan(ip_addr, port)
+port = 66
+targets = input(colored('[+] Enter target/s to scan (split targets with ","): ', 'blue'))
+if ',' in targets:
+    for ip_addr in targets.split(','):
+        scan(ip_addr.strip(), port)
+else:
+    scan(targets.strip(),port)
+
+
