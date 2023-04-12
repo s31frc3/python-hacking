32474e758e4b459258e596bd13fb1357e20621e4
Andrei <zenin.an@do-student.ru>
2023-04-12 07:46:49 +0300

    added port selection and banners

--- port_scanner.py
+++ port_scanner.py
@@ -5,7 +5,7 @@ from progress.bar import IncrementalBar
 
 def scan(target, port):
     converted_ip = check_ip(target)
-    print(colored(f'\n scanning target {str(target)}', 'yellow'))
+    print(colored(f'\nscanning target {str(target)}', 'yellow'))
     with IncrementalBar('Scanning ports:', max=port) as bar:
         for port in range(1, port):
             scan_port(converted_ip, port, bar)
@@ -17,12 +17,20 @@ def check_ip(ip):
     except ValueError:
         return s.gethostbyname(ip)
 
+def get_banner(socket):
+    return socket.recv(1024)
+
 def scan_port(ip_addr, port, bar):
     try:
         sock = s.socket()
-        sock.settimeout(0.5)
+        sock.settimeout(0.1)
         sock.connect((ip_addr, port))
-        print(colored(f'\n[+] Port {port} is open!', 'green'))
+        try:
+            banner = get_banner(sock)
+            print(colored(f'''\n[+] Port {port} is open!
+            {str(banner.decode())}''', 'green'))
+        except:
+            print(colored(f'\n[+] Port {port} is open!', 'green'))
         sock.close()
     except:
         # print(colored(f'[-] Port {port} is closed', 'red'))
@@ -30,8 +38,20 @@ def scan_port(ip_addr, port, bar):
     finally:
         bar.next()
 
-port = 66
 targets = input(colored('[+] Enter target/s to scan (split targets with ","): ', 'blue'))
+port_st = 0
+while port_st not in ['1','2','3','4']:
+    port_st = input(colored('''what ports u want to scan?:
+1) 1-100, 2) 1-1024, 3) 1-65535 4) your num: ''', 'blue'))
+    if port_st == '1':
+        port = 100
+    elif port_st == '2':
+        port = 1024
+    elif port_st == '3':
+        port = 65535
+    elif port_st == '4':
+        port = int(input(colored('enter max port num: ', 'yellow')))
+
 if ',' in targets:
     for ip_addr in targets.split(','):
         scan(ip_addr.strip(), port)
