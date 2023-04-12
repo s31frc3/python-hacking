d1fc73fad744db52ed0422bd2c22a67202ba540b
Andrei <zenin.an@do-student.ru>
2023-04-12 19:12:03 +0300

    f

--- vulnscanner/vulnscanner.py
+++ vulnscanner/vulnscanner.py
@@ -7,3 +7,12 @@ print('\n')
 
 target = portscanner.port_scan(targets_ip, port_number)
 target.scan()
+
+with open(vul_file, 'r') as file:
+    count = 0
+    for banner in target.banners:
+        file.seek(0) #from beginning
+        for line in file.readlines():
+            if line.strip() in banner:
+                print(f'[!] vulnerable banner: {banner} on port {str(target.open_ports[count])}')
+    count += 1
